51732a378000eee7f2db903f4bf6bf3bced360b5
DinamohammadFe <dinamohammad212@gmail.com>
2025-07-14 19:48:14 +0300

    Add WelcomeMessage component and use in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
+    <div className="App">
+      <WelcomeMessage />
     </div>
   );
 }
