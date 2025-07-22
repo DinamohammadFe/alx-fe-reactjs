863f171e075e2465b99409dac0b68427b87199d8
DinamohammadFe <dinamohammad212@gmail.com>
2025-07-22 17:05:58 +0300

    Add App.jsx and components for routing

--- my-company/src/app.jsx
+++ my-company/src/app.jsx
@@ -8,15 +8,13 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
