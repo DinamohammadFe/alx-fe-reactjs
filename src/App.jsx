9c3a8d9e198ee193903439d558df5f65d21beae5
DinamohammadFe <dinamohammad212@gmail.com>
2025-07-27 22:20:05 +0300

    Include package.json and all project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
-=======
 import { Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
->>>>>>> 1c46259c63bf2d1c53f01c0b50f0a2e85bb51928
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Recipe Sharing App</h1>
-<<<<<<< HEAD
-      <AddRecipeForm />
-      <RecipeList />
-=======
       <Routes>
         <Route
           path="/"
@@ -28,7 +19,6 @@ function App() {
         />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
->>>>>>> 1c46259c63bf2d1c53f01c0b50f0a2e85bb51928
     </div>
   );
 }
