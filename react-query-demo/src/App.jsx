c98e76f2d79274ee6e5a8277da684b798bfd564d
DinamohammadFe <dinamohammad212@gmail.com>
2025-08-23 15:29:24 +0300

    Setup React Query in App component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,15 +1,20 @@
+// src/App.jsx
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
-import RefetchButton from "./components/RefetchButton";
 
-const App = () => {
+// Create a client
+const queryClient = new QueryClient();
+
+function App() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>React Query Demo</h1>
-      <RefetchButton />
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
-};
+}
 
 export default App;
